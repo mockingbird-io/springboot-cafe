4b5fc59162575ebf676c7433ea3ef109cee93eb1
The Muse <lazy@TheMusedeMacBook-Pro.local>
2021-08-23 18:39:31 +0800

    update

---
+++ src/test/java/MockingBird/Springbootcafe/SpringbootCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package MockingBird.Springbootcafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
