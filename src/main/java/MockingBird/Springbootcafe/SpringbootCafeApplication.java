e368ae7cbb9679a68a1dfebc52a4a91a0dddeb3d
The Muse <lazy@TheMusedeMacBook-Pro.local>
2021-09-05 14:07:39 +0800

    update

--- src/main/java/MockingBird/Springbootcafe/SpringbootCafeApplication.java
+++ src/main/java/MockingBird/Springbootcafe/SpringbootCafeApplication.java
@@ -13,6 +13,7 @@ public class SpringbootCafeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootCafeApplication.class, args);
 	}
+
 	@GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
