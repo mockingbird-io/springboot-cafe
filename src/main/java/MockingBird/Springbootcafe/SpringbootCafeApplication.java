4a038be44a4cfb53f3a132d96bc3993cf9a82af8
The Muse <lazy@ThedeMacBook-Pro.local>
2021-12-03 23:45:37 +0800

    update

--- src/main/java/MockingBird/Springbootcafe/SpringbootCafeApplication.java
+++ src/main/java/MockingBird/Springbootcafe/SpringbootCafeApplication.java
@@ -15,7 +15,7 @@ public class SpringbootCafeApplication {
 	}
 
 	@GetMapping("/hello")
-    public String hello(@RequestParam(value = "lazy", defaultValue = "World") String name) {
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
     }
 }
