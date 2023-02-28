20f215fa8fb726e53b37a0cf08d4ee32492ff231
Mockingbird-io <mockingbirdccc@gmail.com>
2023-02-28 20:13:04 +0800

    update

--- src/main/java/com/mockingbird/tmall/config/CORSConfiguration.java
+++ src/main/java/com/mockingbird/tmall/config/CORSConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CORSConfiguration implements WebMvcConfigurer {
+
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		//所有请求都允许跨域
