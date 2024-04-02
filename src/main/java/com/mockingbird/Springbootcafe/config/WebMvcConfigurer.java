7fdd5cbfe32142e3f57a1c183a31fbacdc97b9c1
Mockingbird-io <mockingbirdccc@gmail.com>
2024-04-02 09:48:58 +0800

    update

---
+++ src/main/java/com/mockingbird/Springbootcafe/config/WebMvcConfigurer.java
@@ -0,0 +1,22 @@
+package com.mockingbird.Springbootcafe.config;
+
+import com.mockingbird.Springbootcafe.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+class WebMvcConfigurers implements WebMvcConfigurer {
+
+    @Bean
+    public LoginInterceptor getLoginIntercepter() {
+        return new LoginInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(getLoginIntercepter())
+                .addPathPatterns("/**");
+    }
+}
