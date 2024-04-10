622e22760be4468f1fe7d6caad89f0fdae36171c
Mockingbird-io <mockingbirdccc@gmail.com>
2024-04-10 21:01:00 +0800

    update

--- src/main/java/com/mockingbird/Springbootcafe/config/WebMvcConfigurers.java
+++ src/main/java/com/mockingbird/Springbootcafe/config/WebMvcConfigurers.java
@@ -1,6 +1,7 @@
 package com.mockingbird.Springbootcafe.config;
 
 import com.mockingbird.Springbootcafe.interceptor.LoginInterceptor;
+import com.mockingbird.Springbootcafe.interceptor.OtherInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -10,12 +11,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 class WebMvcConfigurers implements WebMvcConfigurer {
 
     @Bean
+    public OtherInterceptor getOtherIntercepter() {
+        return new OtherInterceptor();
+    }
+
+    @Bean
     public LoginInterceptor getLoginIntercepter() {
         return new LoginInterceptor();
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(getOtherIntercepter())
+                .addPathPatterns("/**");
         registry.addInterceptor(getLoginIntercepter())
                 .addPathPatterns("/**");
     }
---
+++ src/main/java/com/mockingbird/Springbootcafe/interceptor/OtherInterceptor.java
@@ -0,0 +1,50 @@
+package com.mockingbird.Springbootcafe.interceptor;
+
+import com.mockingbird.Springbootcafe.pojo.Category;
+import com.mockingbird.Springbootcafe.pojo.OrderItem;
+import com.mockingbird.Springbootcafe.pojo.Users;
+import com.mockingbird.Springbootcafe.service.CategoryService;
+import com.mockingbird.Springbootcafe.service.OrderItemService;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+public class OtherInterceptor implements HandlerInterceptor {
+    @Resource
+    CategoryService categoryService;
+    @Resource
+    OrderItemService orderItemService;
+    @Resource
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        return true;
+    }
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+        HttpSession session = httpServletRequest.getSession();
+        Users user =(Users) session.getAttribute("user");
+        int  cartTotalItemNumber = 0;
+        if(null!=user) {
+            List<OrderItem> ois = orderItemService.listByUsers(user);
+            for (OrderItem oi : ois) {
+                cartTotalItemNumber+=oi.getNumber();
+            }
+
+        }
+
+        List<Category> cs =categoryService.list();
+        String contextPath=httpServletRequest.getServletContext().getContextPath();
+
+        httpServletRequest.getServletContext().setAttribute("categories_below_search", cs);
+        session.setAttribute("cartTotalItemNumber", cartTotalItemNumber);
+        httpServletRequest.getServletContext().setAttribute("contextPath", contextPath);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+    }
+}
