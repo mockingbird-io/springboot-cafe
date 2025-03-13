3d9c7129997ff59b29fe21dafd6f9f1f7bdfc760
Mockingbird-io <mockingbirdccc@gmail.com>
2025-03-13 14:17:03 +0800

    set redis

--- src/main/java/com/mockingbird/Springbootcafe/service/ReviewService.java
+++ src/main/java/com/mockingbird/Springbootcafe/service/ReviewService.java
@@ -3,24 +3,31 @@ package com.mockingbird.Springbootcafe.service;
 import com.mockingbird.Springbootcafe.dao.ReviewDAO;
 import com.mockingbird.Springbootcafe.pojo.Product;
 import com.mockingbird.Springbootcafe.pojo.Review;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.List;
 
 @Service
+@CacheConfig(cacheNames = "reviews")
 public class ReviewService {
     @Resource
     ReviewDAO reviewDAO;
 
+    @CacheEvict(allEntries = true)
     public void add(Review review) {
         reviewDAO.save(review);
     }
 
+    @Cacheable(key="'reviews-pid-'+ #p0.id")
     public List<Review> list(Product product) {
         return reviewDAO.findByProductOrderByIdDesc(product);
     }
 
+    @Cacheable(key="'reviews-count-pid-'+ #p0.id")
     public int getCount(Product product) {
         return reviewDAO.countByProduct(product);
     }
--- src/main/java/com/mockingbird/Springbootcafe/service/UserService.java
+++ src/main/java/com/mockingbird/Springbootcafe/service/UserService.java
@@ -4,6 +4,9 @@ package com.mockingbird.Springbootcafe.service;
 import com.mockingbird.Springbootcafe.dao.UserDAO;
 import com.mockingbird.Springbootcafe.pojo.Users;
 import com.mockingbird.Springbootcafe.util.Page4Navigator;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -14,10 +17,12 @@ import javax.annotation.Resource;
 
 
 @Service
+@CacheConfig(cacheNames="users")
 public class UserService {
     @Resource
     UserDAO userDAO;
 
+    @Cacheable(key="'users-page-'+#p0+ '-' + #p1")
     public Page4Navigator<Users> list(int start, int size, int navigatePages) {
         Pageable pageable = PageRequest.of(start, size, Sort.by(Sort.Direction.DESC, "id"));
         Page<Users> pageFromJPA = userDAO.findAll(pageable);
@@ -29,14 +34,17 @@ public class UserService {
         return users != null;
     }
 
+    @CacheEvict(allEntries=true)
     public void add(Users users) {
         userDAO.save(users);
     }
 
+    @Cacheable(key="'users-one-name-'+ #p0 +'-password-'+ #p1")
     public Users get(String name, String password) {
         return userDAO.getByNameAndPassword(name, password);
     }
 
+    @Cacheable(key="'users-one-name-'+ #p0")
     public Users getByName(String name) {
         return userDAO.findByName(name);
     }
